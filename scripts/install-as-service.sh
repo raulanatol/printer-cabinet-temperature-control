08c5b8632d74b12858415061fa95af0f1a950cc7
raulanatol <raul@natol.es>
2023-07-10 17:58:07 +0100

    Update scripts

--- scripts/install-as-service.sh
+++ scripts/install-as-service.sh
@@ -4,10 +4,10 @@ set -eu
 
 echo "Installing cabinet-control as service"
 
-sudo ln -s ./settings/cabinet-control.service /lib/systemd/system/cabinet-control.service
+sudo cp settings/cabinet-control.service /lib/systemd/system
 sudo chmod +x /lib/systemd/system/cabinet-control.service
 sudo systemctl daemon-reload
-sudo systemctl enable cabinet-control.sevice
+sudo systemctl enable cabinet-control.service
 sudo systemctl start cabinet-control.service
 
 echo "Installed 🤘!"
