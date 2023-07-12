56e9b16edac58152ff47ff7f1bac0624cb711489
raulanatol <raul@natol.es>
2023-07-12 18:13:03 +0100

    Testing

--- src/modules/Fan.py
+++ src/modules/Fan.py
@@ -1,5 +1,7 @@
 from enum import Enum
+from gpiozero import LED
 
+fan = LED(11)
 
 class FanStatus(Enum):
     STOP = 0
@@ -13,14 +15,17 @@ class Fan:
     def start_high(self):
         if self.current_status != FanStatus.HIGH:
             print("Starting fan - High")
+            fan.on()
             self.current_status = FanStatus.HIGH
 
     def start_medium(self):
         if self.current_status != FanStatus.MEDIUM:
             print("Starting fan - Medium")
+            fan.on()
             self.current_status = FanStatus.MEDIUM
 
     def stop(self):
         if self.current_status != FanStatus.STOP:
             print("Stopping fan")
+            fan.off()
             self.current_status = FanStatus.STOP
