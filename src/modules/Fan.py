0c931ad534414aea410137f2cc6fae7f12710793
raulanatol <raul@natol.es>
2023-07-16 17:38:46 +0100

    Update

--- src/modules/Fan.py
+++ src/modules/Fan.py
@@ -2,20 +2,20 @@ import RPi.GPIO as GPIO
 
 fan_power_switch_pin = 17
 
-GPIO.setup(fan_power_switch_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(fan_power_switch_pin, GPIO.OUT, initial=GPIO.HIGH)
 
 
 class Fan:
     is_running = False
-    
+
     def stop(self):
         if self.is_running:
             print("Stopping fan")
-            GPIO.output(fan_power_switch_pin, GPIO.LOW)
+            GPIO.output(fan_power_switch_pin, GPIO.HIGH)
             self.is_running = False
 
     def start(self):
         if not self.is_running:
             print("Starting fan")
-            GPIO.output(fan_power_switch_pin, GPIO.HIGH)
+            GPIO.output(fan_power_switch_pin, GPIO.LOW)
             self.is_running = True
