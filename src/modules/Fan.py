9e21d95f2852f9eee294c0c737cfa97a603c351e
raulanatol <raul@natol.es>
2023-07-16 16:24:16 +0100

    Update fan pines

--- src/modules/Fan.py
+++ src/modules/Fan.py
@@ -1,15 +1,16 @@
 from enum import Enum
-from gpiozero import LED
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+fan_speed_pin = 24
+fan_power_switch_pin = 11
+
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+GPIO.setup(fan_speed_pin, GPIO.OUT)
+GPIO.setup(fan_power_switch_pin, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.setup(24, GPIO.OUT)
-fan_speed = GPIO.PWM(24, 100)
-fan_power_switch = LED(11)
+fan_speed = GPIO.PWM(fan_speed_pin, 100)
 fan_speed.start(0)
-
 
 class FanStatus(Enum):
     STOP = 0
@@ -24,19 +25,19 @@ class Fan:
         if self.current_status != FanStatus.HIGH:
             print("Starting fan - High")
             fan_speed.ChangeDutyCycle(100)
-            fan_power_switch.on()
+            GPIO.output(fan_power_switch_pin, GPIO.HIGH)
             self.current_status = FanStatus.HIGH
 
     def start_medium(self):
         if self.current_status != FanStatus.MEDIUM:
             print("Starting fan - Medium")
             fan_speed.ChangeDutyCycle(50)
-            fan_power_switch.on()
+            GPIO.output(fan_power_switch_pin, GPIO.HIGH)
             self.current_status = FanStatus.MEDIUM
 
     def stop(self):
         if self.current_status != FanStatus.STOP:
             print("Stopping fan")
-            fan_power_switch.off()
+            GPIO.output(fan_power_switch_pin, GPIO.LOW)
             fan_speed.ChangeDutyCycle(0)
             self.current_status = FanStatus.STOP
